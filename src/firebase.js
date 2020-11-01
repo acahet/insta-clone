cb6ea65f41d25b051b1c9f356d8a9a9c4a49f54f
anderson-cahet <anderson.cahet@ghe.megaleo.com>
2020-11-01 23:21:24 +0000

    feat: setup firebase
    - add env file
    - env to gitignore

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import firebase from 'firebase';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseApp = firebase.initializeApp({
-	apiKey: 'AIzaSyCBxch5c2fkeEYXgBrNRf35zGzoQS826PE',
-	authDomain: 'react-insta-clone-9725a.firebaseapp.com',
-	databaseURL: 'https://react-insta-clone-9725a.firebaseio.com',
-	projectId: 'react-insta-clone-9725a',
-	storageBucket: 'react-insta-clone-9725a.appspot.com',
-	messagingSenderId: '890278582496',
-	appId: '1:890278582496:web:928f50dd0c74016bcf1885',
-	measurementId: 'G-06S3T47Q24',
+	apiKey: process.env.REACT_APP_FIREBASE_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+	databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+	appId: process.env.REACT_APP_FIREBASE_APP_ID,
+	measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 });
 
 const db = firebaseApp.firestore();
